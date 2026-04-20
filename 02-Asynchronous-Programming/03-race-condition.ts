cb00534c2d634a4062ed88d00bab1c1139e6fed3
曾凌枫 <19802021903@139.com>
2026-04-20 14:16:20 +0800

    fix: resolve async race conditions using AbortController

--- 02-Asynchronous-Programming/03-race-condition.ts
+++ 02-Asynchronous-Programming/03-race-condition.ts
@@ -1,16 +1,25 @@
-let globleRequestId = 0;
+let globlecontroller: AbortController | null = null;
 
 const fetchRes = async (id: number) => {
-    const currentID = ++globleRequestId
+    if (globlecontroller) {
+        globlecontroller.abort()
+    }
     console.log(`#${id} start`)
+    const currentcontroller = new AbortController()
+    globlecontroller = currentcontroller
+    const { signal } = globlecontroller;
 
     let delay = id == 1 ? 2000 : 500
-    await new Promise(resolve => setTimeout(resolve, delay))
+    await new Promise((resolve, reject) => {
+        const timer = setTimeout(resolve, delay);
+        signal.addEventListener('abort', () => {
+            clearTimeout(timer);
+            reject(new Error('Aborted'));
+        });
+    });
 
-    if (currentID !== globleRequestId) {
-        console.log(`#${id} out of time `)
-        return;
-    }
+    if (signal.aborted) return;
+
     console.log(`Request #${id} successfully updated the UI!`)
 
 }
