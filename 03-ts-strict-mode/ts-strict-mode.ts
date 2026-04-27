4f2634880744301b1afa17e6302322f3c0cb0bb9
曾凌枫 <19802021903@139.com>
2026-04-27 16:40:30 +0800

    feat: simplify complex logic with Discriminated Unions

--- 03-ts-strict-mode/ts-strict-mode.ts
+++ 03-ts-strict-mode/ts-strict-mode.ts
@@ -1,20 +1,21 @@
-interface lululemonMember {
-    name: string,
-    vipNumber?: {
-        discount: number
+interface loadingState { type: 'loading' }
+interface successState { type: 'success'; data: string[] }
+interface falseState { type: 'false'; message: string }
+
+type fetchState = loadingState | successState | falseState
+
+const renderUI = (state: fetchState) => {
+    switch (state.type) {
+        case 'loading':
+            console.log('loading')
+            break;
+        case 'success':
+            console.log(state.data.join(','))
+            break;
+        case 'false':
+            console.log(state.message)
+            break;
+        default:
+            break;
     }
-}
-
-const getDiscount = (menber: lululemonMember) => {
-    // ❌
-    // console.log(menber.vipNumber.discount)
-
-    console.log(menber.vipNumber?.discount)
-
-    if (menber.vipNumber) {
-        console.log(menber.vipNumber.discount)
-    } else {
-        console.log("not discount")
-    }
-
 }
